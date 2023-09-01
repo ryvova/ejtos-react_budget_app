fc939d5718267be23e99e460d5cfd7196ca33cfb
ryvova <anna.ryvova@gmail.com>
2023-09-01 23:54:50 +0200

    Update Budget.js
    Signed-off-by: ryvova <anna.ryvova@gmail.com>

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,11 +1,28 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
+
+const changeValue = () => {
+    if(budget > remaining) {
+        alert("The value cannot exceed remaining funds  £" + remaining);
+        setCost("");
+        return;
+    }
+
+    if(budget < totalExpenses) {
+        alert("You cannot reduce the budget value lower than the spending");
+        setCost("");
+        return;
+    }
+}
+
 const Budget = () => {
     const { budget } = useContext(AppContext);
     return (
         <div className='alert alert-secondary'>
             <span>
-                Budget:  £<input type="number" min="0" max="2000" step="10" valuer={budget} />
+                Budget:  
+                <label htmlFor="budget" className="currencySymbol">£</label>
+                <input id="budget" type="number" min="0" max="20000" onChange={changeValue} />
             </span>
         </div>
     );
