ea14e912324d9cf1e9afbaeed1df922ec9edd9ea
ryvova <anna.ryvova@gmail.com>
2023-09-01 23:57:17 +0200

    Update Currency.js
    Signed-off-by: ryvova <anna.ryvova@gmail.com>

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,17 +1,24 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+
+const changeValue = (value) => {
+  for (let i = 0; i < document.getElementsByClassName("currencySymbol").length; i++) {
+    document.getElementsByClassName("currencySymbol")[i].innerText = value;
+  }
+}
 
 const Currency = () => {
     const {currency} = useContext(AppContext);
     return (
         <div className='alert alert-currency'>
-            <select id="currency" name="currency" class="currency" onfocus="this.size=6;"
-  onblur="this.size=0;"
-  onchange="this.size=1; this.blur()">
-                <option value="dollar">$ Dollar</option>
-                <option value="pound">£ Pound</option>
-                <option value="euro">€ Euro</option>
-                <option value="ruppee">₹ Ruppee</option>
+            <select id="currency" name="currency" className="currency" defaultValue="£"
+                onChange={(event) => changeValue(event.target.value)} >
+                <option value="$">$ Dollar</option>
+                <option value="£">£ Pound</option>
+                <option value="€">€ Euro</option>
+                <option value="₹">₹ Ruppee</option>
             </select>
         </div>
     );
